4aba5457f076f62b403a4dee42c8dca9bd5f6598
unknown <luke.riechert@rams.colostate.edu>
2021-12-04 15:11:27 -0700

    15.1 12/4 Vuex, login

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import {createStore} from 'vuex';
+
+export default createStore({
+    state: {
+        token: null,
+        user: null,
+        players: []
+    },
+    mutations: {
+        storeTokenInApp(state, myToken){
+            state.token = myToken;
+        },
+        storeUserInApp(state, theUser){
+            state.user = theUser;
+        },
+    },
+    actions: {},
+});
