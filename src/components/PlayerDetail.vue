9be1d3760c90954b458b90298b9582b8979175be
unknown <luke.riechert@rams.colostate.edu>
2021-12-05 18:53:19 -0700

    15.2 12/5 /movie/:pk

--- src/components/PlayerDetail.vue
+++ src/components/PlayerDetail.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
-      <h1>Player Details</h1>
+      <div class="card">
+        <div class="card-body">
+          <h2 class="text-primary">{{player.FName}} {{player.LName}}</h2>
+          <br/>
+
+          <p>Position: <br/><strong>{{player.PositionName}}</strong></p>
+          <p>Position Number: <br/><strong>{{player.PositionFK}}</strong></p>
+        </div>
+      </div>
+      <br/>
+
+      <router-link v-if="auth" :to="`/players/${this.$route.params.pk}/report`"><button class="btn btn-success">Add a Report</button></router-link>
+
+      <router-link v-else :to="`/login`"><button class="btn btn-outline-success">Sign In to Add a Report</button></router-link>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    player(){
+      let allPlayers = this.$store.state.players;
 
-}
+      let thisPlayer = allPlayers.find((aPlayer)=>{
+        return aPlayer.PlayerPK == this.$route.params.pk;
+      });
+
+      return thisPlayer;
+    },
+    auth(){return this.$store.state.token},
+  },
+};
 </script>
 
 <style>
